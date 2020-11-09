3dd57e95398ca6be6ee44f07c7f62c7b3d478432
Geoffrey.C <cycjimmy@gmail.com>
2020-11-09 17:29:19 +0800

    chore(*): add debug page

--- rollup/rollup.config.dev.js
+++ rollup/rollup.config.dev.js
@@ -1,6 +1,9 @@
-import {input, name, plugins, exports} from './rollup.common';
+import browsersync from 'rollup-plugin-browsersync';
+import makeAjax from '@cycjimmy/config-lib/browsersync/2.x/middleware/makeAjax';
 
 import pkg from '../package.json';
+
+import {input, IS_DEVELOPMENT, name, plugins} from './rollup.common';
 
 export default [
   {
@@ -8,11 +11,58 @@ export default [
     output: {
       name,
       file: pkg.browser.replace('.min.js', '.js'),
-      format: 'umd',
-      exports,
+      format: 'umd'
     },
     plugins: [
       ...plugins,
-    ],
-  },
+
+      IS_DEVELOPMENT &&
+      browsersync({
+        server: ['static', 'dist'],
+        watch: true,
+        middleware: [
+          {
+            route: '/getSuccess',
+            handle: makeAjax({
+              apiName: 'getSuccess',
+              data: {
+                'massage': 'success'
+              },
+              timeout: 500,
+            })(),
+          },
+          {
+            route: '/postSuccess',
+            handle: makeAjax({
+              apiName: 'postSuccess',
+              data: {
+                'massage': 'success'
+              },
+              timeout: 500,
+            })(),
+          },
+          {
+            route: '/scriptSuccess',
+            handle: makeAjax({
+              apiName: 'scriptSuccess',
+              data: JSON.stringify({
+                'massage': 'success'
+              }),
+              timeout: 500,
+            })(),
+          },
+          {
+            route: '/jsonpSuccess',
+            handle: makeAjax({
+              apiName: 'jsonpSuccess',
+              data: JSON.stringify({
+                'massage': 'success'
+              }),
+              timeout: 500,
+            })(),
+          },
+        ],
+      })
+    ]
+  }
 ];
